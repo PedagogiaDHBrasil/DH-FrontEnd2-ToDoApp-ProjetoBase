e2023eae754d8420786b4dcee1c156567f8ae2ad
ThribsClube <thiago@clubedochaveiro.com.br>
2022-04-05 20:47:54 -0300

    erro de CORS

--- scripts/autenticacao.js
+++ scripts/autenticacao.js
@@ -1,12 +1,50 @@
-const usuario = document.getElementById("inputEmail");
-const senha = document.getElementById("inputPassword");
-const botao = document.getElementById("submit");
-botao.addEventListener("click", function() {
-    if (usuario.value.length>0 && senha.value.length>0) {
-        console.log("usuário logado")
-    }
-    else{
+const usuario = document.getElementById("inputEmail")
+const senha = document.getElementById("inputPassword")
+const botao = document.getElementById("submit")
 
-        console.log("Por favor insira usuário e senha")
-    };
+// const BASE_URL = "https://ctd-todo-api.herokuapp.com/v1"
+
+
+const logarUsuario = function () {
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: {
+            "email": usuario.value,
+            "password": senha.value
+        }
+    }
+    fetch("https://ctd-todo-api.herokuapp.com/v1/users/login", options)
+        .then(response => {
+            console.log(response);
+        })
+}
+
+const criarUsuario = () => {
+    fetch("https://ctd-todo-api.herokuapp.com/v1/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: {
+            "email": usuario.value,
+            "password": senha.value
+        }
+    })
+        .then(response => {
+            console.log(response);
+        })
+}
+
+botao.addEventListener("click", function (event) {
+    event.preventDefault()
+    if (usuario.value.length > 0 && senha.value.length > 0) {
+        logarUsuario()
+    }
+    else {
+        alert("Por favor insira usuário e senha")
+    }
+
 })
