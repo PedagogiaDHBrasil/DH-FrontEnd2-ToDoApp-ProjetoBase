d14c9d153f2c38e0c2ca13ec9923645812e691ca
ThribsClube <thiago@clubedochaveiro.com.br>
2022-04-06 19:29:08 -0300

    Revert "erro de CORS"
    This reverts commit e2023eae754d8420786b4dcee1c156567f8ae2ad.

--- scripts/autenticacao.js
+++ scripts/autenticacao.js
@@ -1,50 +1,12 @@
-const usuario = document.getElementById("inputEmail")
-const senha = document.getElementById("inputPassword")
-const botao = document.getElementById("submit")
-
-// const BASE_URL = "https://ctd-todo-api.herokuapp.com/v1"
-
-
-const logarUsuario = function () {
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: {
-            "email": usuario.value,
-            "password": senha.value
-        }
+const usuario = document.getElementById("inputEmail");
+const senha = document.getElementById("inputPassword");
+const botao = document.getElementById("submit");
+botao.addEventListener("click", function() {
+    if (usuario.value.length>0 && senha.value.length>0) {
+        console.log("usuário logado")
     }
-    fetch("https://ctd-todo-api.herokuapp.com/v1/users/login", options)
-        .then(response => {
-            console.log(response);
-        })
-}
+    else{
 
-const criarUsuario = () => {
-    fetch("https://ctd-todo-api.herokuapp.com/v1/users", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: {
-            "email": usuario.value,
-            "password": senha.value
-        }
-    })
-        .then(response => {
-            console.log(response);
-        })
-}
-
-botao.addEventListener("click", function (event) {
-    event.preventDefault()
-    if (usuario.value.length > 0 && senha.value.length > 0) {
-        logarUsuario()
-    }
-    else {
-        alert("Por favor insira usuário e senha")
-    }
-
+        console.log("Por favor insira usuário e senha")
+    };
 })
