ce46ef93892b2685471692a0a90c85d7fe8df4a2
Juliana-Ruama <91511787+Juliana-Ruama@users.noreply.github.com>
2022-04-06 20:01:07 -0300

    ToDoApp

--- scripts/autenticacao.js
+++ scripts/autenticacao.js
@@ -1,12 +1,13 @@
 const usuario = document.getElementById("inputEmail");
 const senha = document.getElementById("inputPassword");
 const botao = document.getElementById("submit");
-botao.addEventListener("click", function() {
-    if (usuario.value.length>0 && senha.value.length>0) {
-        console.log("usuário logado")
+botao.addEventListener("click", function(event) {
+    event.preventDefault()
+    if (usuario.value.length > 0 && senha.value.length > 0) {
+        LogarUsuario()
     }
     else{
 
-        console.log("Por favor insira usuário e senha")
+        alert("Por favor insira usuário e senha")
     };
 })
