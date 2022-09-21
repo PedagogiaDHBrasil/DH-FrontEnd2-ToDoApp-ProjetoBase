9f68975fc6f85527385a706e38c616659d70049b
GuiMoreiraC <guilherme.moreira.c@protonmail.com>
2022-09-20 23:36:19 -0300

    signup agr vai

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,34 @@
-window.onload = event => {
-    /* ---------------------- obtenemos variables globales ---------------------- */
-    const form = document.forms[0];
-    const email = document.querySelector('#inputEmail');
-    const password = document.querySelector('#inputPassword');
-    const url = 'https://ctd-todo-api.herokuapp.com/v1/users/login';
+window.onload = (event) => {
+  const url = "https://ctd-todo-api.herokuapp.com/v1/users/login",
+    form = document.forms[0];
 
-    /* ------------------- escuchamos el submit del formulario ------------------ */
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        //creamos el cuerpo de la request
-        const payload = {
-            email: email.value,
-            password: password.value,
-        };
-        //configuramos la request del Fetch
-        const settings = {
-            method: 'POST',
-            body: JSON.stringify(payload),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        };
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const payload = {
+        email: form["inputEmail"].value,
+        password: form["inputPassword"].value,
+      },
+      settings = {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: { "Content-Type": "application/json" },
+      };
 
-        /* ---------------------- lanzamos la peticion a la API --------------------- */
-        console.log('Lanzando la consulta a la API');
-        fetch(url, settings)
-            .then(response => response.json())
-            .then(data => {
-                if (data.jwt) {
-                    //armo un objeto literal con cierta info que quiero guardar en LocalStorage
-                    const usuario = {
-                        jwt: data.jwt,
-                        name: email.value.split('@')[0],
-                    };
-                    //guardo en LocalStorage el objeto con el token y el nombre del usuario
-                    localStorage.setItem('user', JSON.stringify(usuario));
-
-                    //redireccionamos a la página
-                    location.replace('/');
-                }
-                //limpio los campos del formulario
-                form.reset();
-            });
-    });
+    console.log("consultando API");
+    fetch(url, settings)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.jwt) {
+          localStorage.setItem(
+            "user",
+            JSON.stringify({
+              jwt: data.jwt,
+              name: form["inputEmail"].value.split("@")[0],
+            })
+          );
+          location.replace("tarefas.html");
+        }
+        // form.reset();
+      });
+  });
 };
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,46 +1,36 @@
-window.onload = event => {
-    const url = 'https://ctd-todo-api.herokuapp.com/v1/users',
-        form = document.forms[0],
-        inputName = document.querySelector('#inputName'),
-        lastName = document.querySelector('#inputLastName'),
-        email = document.querySelector('#inputEmail'),
-        password = document.querySelector('#inputPassword'),
-        passwordConfirm = document.querySelector('#inputPasswordConfirm'),
-        passwordTest = password != passwordConfirm;
+window.onload = (event) => {
+  const url = "https://ctd-todo-api.herokuapp.com/v1/users",
+    form = document.forms[0];
 
-    if (password != passwordConfirm) {
-    }
+  if (form["inputPassword"] != form["inputPasswordConfirm"]) {
+  }
 
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        const payload = {
-                firstName: inputName.value,
-                lastName: lastName.value,
-                email: email.value,
-                password: password.value,
-            },
-            settings = {
-                method: 'POST',
-                body: JSON.stringify(payload),
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            };
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const payload = {
+        firstName: form["inputName"].value,
+        lastName: form["inputLastName"].value,
+        email: form["inputEmail"].value,
+        password: form["inputPassword"].value,
+      },
+      settings = {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: { "Content-Type": "application/json" },
+      };
 
-        console.log('Lanzando la consulta a la API');
-        fetch(url, settings)
-            .then(response => response.json())
-            .then(data => {
-                if (data.jwt) {
-                    const usuario = {
-                        jwt: data.jwt,
-                        name: inputName.value,
-                    };
-                    localStorage.setItem('user', JSON.stringify(usuario));
-
-                    location.replace('tarefas.html');
-                }
-                form.reset();
-            });
-    });
+    fetch(url, settings)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.jwt) {
+          const usuario = {
+            jwt: data.jwt,
+            name: form["inputName"].value,
+          };
+          localStorage.setItem("user", JSON.stringify(usuario));
+          location.replace("tarefas.html");
+        }
+        // form.reset();
+      });
+  });
 };
