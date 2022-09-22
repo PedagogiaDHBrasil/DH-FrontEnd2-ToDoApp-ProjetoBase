a5c41a06b8a43366a14b7f1a5757508d2ce081a3
GuiMoreiraC <guilherme.moreira.c@protonmail.com>
2022-09-22 19:30:09 -0300

    tarefas

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,27 @@
-window.onload = (event) => {
-  const url = "https://ctd-todo-api.herokuapp.com/v1/users/login",
-    form = document.forms[0];
+window.onload = event => {
+    const url = 'https://ctd-todo-api.herokuapp.com/v1/users/login',
+        form = document.forms[0];
 
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const payload = {
-        email: form["inputEmail"].value,
-        password: form["inputPassword"].value,
-      },
-      settings = {
-        method: "POST",
-        body: JSON.stringify(payload),
-        headers: { "Content-Type": "application/json" },
-      };
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const payload = {
+                email: form['inputEmail'].value,
+                password: form['inputPassword'].value,
+            },
+            settings = {
+                method: 'POST',
+                body: JSON.stringify(payload),
+                headers: { 'Content-Type': 'application/json' },
+            };
 
-    console.log("consultando API");
-    fetch(url, settings)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.jwt) {
-          localStorage.setItem(
-            "user",
-            JSON.stringify({
-              jwt: data.jwt,
-              name: form["inputEmail"].value.split("@")[0],
-            })
-          );
-          location.replace("tarefas.html");
-        }
-        // form.reset();
-      });
-  });
+        console.log('consultando API');
+        fetch(url, settings)
+            .then(response => response.json())
+            .then(data => {
+                if (data.jwt) {
+                    localStorage.setItem('userJwt', data.jwt);
+                    location.replace('tarefas.html');
+                }
+            });
+    });
 };
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,36 +1,32 @@
-window.onload = (event) => {
-  const url = "https://ctd-todo-api.herokuapp.com/v1/users",
-    form = document.forms[0];
+window.onload = event => {
+    const url = 'https://ctd-todo-api.herokuapp.com/v1/users',
+        form = document.forms[0];
 
-  if (form["inputPassword"] != form["inputPasswordConfirm"]) {
-  }
-
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const payload = {
-        firstName: form["inputName"].value,
-        lastName: form["inputLastName"].value,
-        email: form["inputEmail"].value,
-        password: form["inputPassword"].value,
-      },
-      settings = {
-        method: "POST",
-        body: JSON.stringify(payload),
-        headers: { "Content-Type": "application/json" },
-      };
-
-    fetch(url, settings)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.jwt) {
-          const usuario = {
-            jwt: data.jwt,
-            name: form["inputName"].value,
-          };
-          localStorage.setItem("user", JSON.stringify(usuario));
-          location.replace("tarefas.html");
-        }
+    if (form['inputPassword'] != form['inputPasswordConfirm']) {
         // form.reset();
-      });
-  });
+    }
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const payload = {
+                firstName: form['inputName'].value,
+                lastName: form['inputLastName'].value,
+                email: form['inputEmail'].value,
+                password: form['inputPassword'].value,
+            },
+            settings = {
+                method: 'POST',
+                body: JSON.stringify(payload),
+                headers: { 'Content-Type': 'application/json' },
+            };
+
+        fetch(url, settings)
+            .then(response => response.json())
+            .then(data => {
+                if (data.jwt) {
+                    localStorage.setItem('userJwt', data.jwt);
+                    location.replace('tarefas.html');
+                }
+            });
+    });
 };
