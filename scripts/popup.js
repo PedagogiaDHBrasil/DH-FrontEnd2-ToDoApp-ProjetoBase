f9a9c29e6bdf3412dce54b50b2d2dc73a8e8e38d
GuiMoreiraC <guilherme.moreira.c@protonmail.com>
2022-09-23 02:17:12 -0300

    popup

---
+++ scripts/popup.js
@@ -0,0 +1,23 @@
+// window.onload = event => {
+const btnFinalizarSessao = document.getElementById('closeApp'),
+    popup = document.querySelector('.popup-wrapper');
+
+btnFinalizarSessao.onclick = event => {
+    popup.style.display = 'block';
+};
+
+popup.onclick = event => {
+    const classNameOfClickElemente = event.target.classList[0],
+        finalizarSessao = 'btn-submit-close' === event.target.classList[0],
+        className = ['popup-close', 'popup-wrapper', 'btn-submit-cancel'],
+        shouldClosePopup = className.some(className => className === classNameOfClickElemente);
+
+    if (finalizarSessao) {
+        localStorage.clear();
+        location.replace('/index.html');
+    }
+    if (shouldClosePopup) {
+        popup.style.display = 'none';
+    }
+};
+// };
--- scripts/tarefa.js
+++ scripts/tarefa.js
@@ -1,88 +1,86 @@
 !localStorage.userJwt ? location.replace('index.html') : '';
 
-window.onload = event => {
-    const urlBase = 'https://ctd-todo-api.herokuapp.com/v1',
-        token = localStorage.userJwt,
-        userName = document.querySelector('.user-name');
-    let userInfo = {};
+const urlBase = 'https://ctd-todo-api.herokuapp.com/v1';
+const token = localStorage.userJwt;
+let userInfo = {};
 
+window.onload = event => {
     dataUser(token);
     consultarListaTasks(token);
-
-    // Dados do usuário
-
-    function dataUser(jwt) {
-        const settings = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                authorization: jwt,
-            },
-        };
-        fetch(urlBase + '/users/getMe', settings)
-            .then(response => response.json())
-            .then(data => {
-                userInfo = {
-                    id: data.id,
-                    firstName: data.firstName,
-                    lastName: data.lastName,
-                    email: data.email,
-                };
-                userName.innerText = userInfo.firstName;
-            });
-    }
-
-    // Listar tarefas
-
-    function consultarListaTasks(jwt) {
-        const settings = {
-            method: 'GET',
-            headers: {
-                authorization: jwt,
-            },
-        };
-        fetch(urlTareas, settings)
-            .then(response => response.json())
-            .then(tasks => {
-                console.log(tasks);
-                listasTasksDom(tasks);
-            });
-    }
-
-    function listasTasksDom(tasksLista) {
-        const tarefasPendentes = document.querySelector('.tarefas-pendentes'),
-            tarefasTerminadas = document.querySelector('.tareas-terminadas'),
-            tarefasPendentesTemplate = '',
-            tarefasTerminadasTemplate = '';
-
-        tasksLista.forEach(task => {
-            let date = new Date(task.createdAt),
-                formatDate = `${date.getUTCFullYear}/${date.getUTCMonth}/${date.getUTCDate}`;
-
-            if (!task.completed) {
-                tarefasPendentesTemplate += `
-                    <li class="tarefa">
-                        <div class="not-done"></div>
-                        <div class="descricao">
-                            <p class="nome">${task.description}</p>
-                            <p class="timestamp">Criada em: ${formatDate}</p>
-                        </div>
-                    </li>
-                    `;
-            } else {
-                tarefasTerminadasTemplate += `
-                    <li class="tarefa">
-                        <div class="not-done"></div>
-                        <div class="descricao">
-                            <p class="nome">${task.description}</p>
-                            <p class="timestamp">Criada em: ${formatDate}</p>
-                        </div>
-                    </li>
-                    `;
-            }
-        });
-
-        tarefasPendentes.innerHTML = tarefasPendentesTemplate;
-        tarefasTerminadas.innerHTML = tarefasTerminadasTemplate;
-    }
 };
+
+// Dados do usuário
+function dataUser(jwt) {
+    const userName = document.querySelector('.user-name');
+    const settings = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            authorization: jwt,
+        },
+    };
+    fetch(urlBase + '/users/getMe', settings)
+        .then(response => response.json())
+        .then(data => {
+            userInfo = {
+                id: data.id,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                email: data.email,
+            };
+            userName.innerText = userInfo.firstName;
+        });
+}
+
+// Listar tarefas
+function consultarListaTasks(jwt) {
+    const settings = {
+        method: 'GET',
+        headers: {
+            authorization: jwt,
+        },
+    };
+    fetch(urlTareas, settings)
+        .then(response => response.json())
+        .then(tasks => {
+            console.log(tasks);
+            listasTasksDom(tasks);
+        });
+}
+
+function listasTasksDom(tasksLista) {
+    const tarefasPendentes = document.querySelector('.tarefas-pendentes'),
+        tarefasTerminadas = document.querySelector('.tareas-terminadas'),
+        tarefasPendentesTemplate = '',
+        tarefasTerminadasTemplate = '';
+
+    tasksLista.forEach(task => {
+        let date = new Date(task.createdAt),
+            formatDate = `${date.getUTCFullYear}/${date.getUTCMonth}/${date.getUTCDate}`;
+
+        if (!task.completed) {
+            tarefasPendentesTemplate += `
+                <li class="tarefa">
+                    <div class="not-done"></div>
+                    <div class="descricao">
+                        <p class="nome">${task.description}</p>
+                        <p class="timestamp">Criada em: ${formatDate}</p>
+                    </div>
+                </li>
+                `;
+        } else {
+            tarefasTerminadasTemplate += `
+                <li class="tarefa">
+                    <div class="not-done"></div>
+                    <div class="descricao">
+                        <p class="nome">${task.description}</p>
+                        <p class="timestamp">Criada em: ${formatDate}</p>
+                    </div>
+                </li>
+                `;
+        }
+    });
+
+    tarefasPendentes.innerHTML = tarefasPendentesTemplate;
+    tarefasTerminadas.innerHTML = tarefasTerminadasTemplate;
+}
